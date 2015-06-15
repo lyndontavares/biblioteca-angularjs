b5fd8add36f9049d06c4febd93178c8d436b2965
lyndontavares <integraldominio@gmail.com>
2015-06-14 21:46:19 -0300

    first commit

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-# MEAN Stack RESTful API Tutorial - Contact List App
-<h2>MEAN Stack RESTful API Tutorial - Contact List App</h2>
-
-<img src="http://i288.photobucket.com/albums/ll175/michaelcheng429/meanstacktutorial_zpsu72ixs47.png" alt="mean stack tutorial app">
-
-This repo contains the code for a RESTful API Contact List App that was built using the MEAN stack:
+# Biblioteca - Datapar
 
 <ul>
 <li>MongoDB</li>
@@ -11,22 +6,6 @@ This repo contains the code for a RESTful API Contact List App that was built us
 <li>AngularJS</li>
 <li>NodeJS</li>
 </ul>
-
-<h3>YouTube Tutorial</h3>
-
-Here is the 5-part YouTube tutorial for this MEAN stack app:
-
-<h4><a href="https://www.youtube.com/watch?v=kHV7gOHvNdk">Part 1/5: Using MongoDB, Express, AngularJS, and NodeJS Together</a></h4>
-
-<h4><a href="https://www.youtube.com/watch?v=7F1nLajs4Eo">Part 2/5: AngularJS and NodeJS App Tutorial</a></h4>
-
-<h4><a href="https://www.youtube.com/watch?v=oVIeMfvgTz8">Part 3/5: MongoDB Tutorial: Downloading, Installing, and Basics</a></h4>
-
-<h4><a href="https://www.youtube.com/watch?v=iFsYJG3fGro">Part 4/5: RESTful API, GET and POST Data</a></h4>
-
-<h4><a href="https://www.youtube.com/watch?v=06_SIzYXgqQ">Part 5/5: RESTful API Tutorial, DELETE and PUT Data</a></h4>
-
-<h3>Instructions</h3>
 
 If you have trouble with the tutorial (especially since I had to modify the code due to an AngularJS update), simply clone this repo using 
 
--- server.js
+++ server.js
@@ -3,22 +3,36 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
+
+//local
+//var db = mongojs('livros', ['livros']);
+
+//user:vipangularjs sen:www010203 
+//var db = mongojs('mongodb://adm:123@ds043972.mongolab.com:43972/contactlist', ['contactlist']);
+
+//user: lyndon tavares sen:www010203
+var db = mongojs( 'mongodb://adm:123@ds051170.mongolab.com:51170/db_mongo', ['livros','usuarios']);
+
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
+
+// livros ////////////////////////////////////////////////////
+
 app.get('/livro', function (req, res) {
-  console.log('I received a GET request');
+  console.log('*****get LIVRO');
 
   db.livros.find(function (err, docs) {
+  //db.contactlist.find(function (err, docs) {
     console.log(docs);
     res.json(docs);
   });
 });
 
 app.post('/livro', function (req, res) {
+  console.log('Adicionado Livro...')  ;
   console.log(req.body);
   db.livros.insert(req.body, function(err, doc) {
     res.json(doc);
@@ -52,6 +66,53 @@ app.put('/livro/:id', function (req, res) {
     }
   );
 });
+
+
+// users ////////////////////////////////////////////////////
+app.get('/user', function (req, res) {
+  console.log('*****get USER');
+ db.usuarios.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/user', function (req, res) {
+  console.log('Adicionado user...')  ;
+  console.log(req.body);
+  db.usuarios.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/user/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.usuarios.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/user/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.usuarios.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/user/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.usuarios.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {nome: req.body.nome, senha: req.body.senha}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+/////////////////////////////////////////////////////////////
 
 app.listen(3000);
 console.log("Server running on port 3000");
